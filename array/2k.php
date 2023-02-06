41b121952a83deacd8da32cd19c9f90713ab344d
кот <ecopolimerdir@gmail.com>
2023-02-06 14:33:27 +0200

    TASKS ARRAY

--- array/2k.php
+++ array/2k.php
@@ -1,9 +1,9 @@
-<?php
+<?php //echo * elem mass
 $a = [2,2,2,2,2];
 $sum=1;
 for ($n = 0; $n <= 4; $n++){
     $sum = $sum * $a[$n];
 
 }
-echo $sum;
-?>
+echo $sum . '<br>';
+?> // произведение элементов массива
