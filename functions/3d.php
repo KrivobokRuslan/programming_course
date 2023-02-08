b46c54eef20351aad2690bb123abd09ed8cff593
Ptushkoanna <Ptushkoanna@ukr.net>
2023-02-08 18:07:12 +0100

    ifdimon

---
+++ functions/3d.php
@@ -0,0 +1,15 @@
+<?php
+//3) попробовать сделать свою функцию аналог функции count()
+
+function q($a) {
+    $i = 0;
+    $maxq = $i++;
+    for($i = 0; $i > 0; $i++){
+    while($i < $maxq){
+        $i++;
+    }
+    if ($i = $maxq){
+        echo $maxq;
+
+    }
+}}
