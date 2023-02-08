d3eafcf730d3769a3a69bfcbebae31b56f5fa36b
кот <ecopolimerdir@gmail.com>
2023-02-08 18:11:45 +0200

    commit message

---
+++ functions/1k.php
@@ -0,0 +1,12 @@
+<?php  //sum elem mass
+$mass = [4, 5, 6];
+    $mass[] = 7;{
+        function sum ($mass){
+            $s = sum(0);
+        for ($i=1; $i < count($mass)); $i++)
+            $s += sum($i);{
+                echo $s;
+            }
+        }
+
+
