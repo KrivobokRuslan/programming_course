fa030b910376f2dde659f25c3595a30e44a12678
krivobokruslan <krivobokruslan@gmail.com>
2023-02-09 19:58:17 +0200

    tasks for functions

--- functions/array/index.php
+++ functions/array/index.php
@@ -51,3 +51,44 @@ echo '<br>';
 echo $v;
 echo '<br>';
 echo $x[$v];
+
+$s = ['first' => 6, 'second' => 7, 'third' => 8, 'fourth' => 9]; // => [0 => 6, 1 => 7, 2 => 8, 3 => 9]
+
+function _array_values(array $array) : array
+{
+    $result = [];
+
+    foreach ($array as $value) {
+        $result[] = $value;
+    }
+
+    return $result;
+}
+
+$ff = _array_values($s); // [0 => 6, 1 => 7, 2 => 8, 3 => 9]
+
+function _array_keys(array $array) : array
+{
+    $result = [];
+
+    foreach ($array as $k => $v) {
+        $result[] = $k;
+    }
+
+    return $result;
+}
+
+$gg = _array_keys($s); // [0 => 'first', 1 => 'second', 2 => 'third', 3 => 'fourth']
+
+/**
+ * 1) Написать функцию, которая возвращает квадрат числа
+ * 2) Написать функцию, которая возвращает
+ * 3) Сделайте функцию, которая отнимает от первого числа второе и делит на третье.
+ * 4) Сделайте функцию, которая принимает параметром число от 1 до 7, а возвращает день недели на русском языке.
+ * 5) _array_flip — Меняет местами ключи с их значениями в массиве (['first' => 6, 'second' => 7, 'third' => 8, 'fourth' => 9]; // => [6 => 'first', 7 => 'second', 8 => 'third', 9 => 'fourth'])
+ * $a = [];
+ * $a[67] = 'sadfgdsf';
+ * $a['start'] = 345;
+ * [67 => 'sadfgdsf', 'start' => 345]
+ * 6) _array_key_exists — Проверяет, присутствует ли в массиве указанный ключ или индекс
+ */
