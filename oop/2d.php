4ccfba46d34a99742837f341102a9e094d23ffd8
Ptushkoanna <Ptushkoanna@ukr.net>
2023-03-06 17:00:17 +0100

    2oop

--- oop/2d.php
+++ oop/2d.php
@@ -1,1 +1,13 @@
 <?php
+class Color {
+    public function showthecolor() {
+        echo "The color is";
+    }
+}
+class Paint {
+    public $color;
+    public function __construct() {
+        $this->color = new Color();
+        echo $this->color->showthecolor();
+    }
+}
