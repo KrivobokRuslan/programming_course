7c364563745141438e64444116cf644bd3fb9c9a
кот <ecopolimerdir@gmail.com>
2023-03-13 18:29:05 +0200

    commit message

--- oop/interfaces/1k.php
+++ oop/interfaces/1k.php
@@ -1,51 +1,31 @@
-<?php
-interface Masage{
-    function send();
-}
-class realise implements Masage{
-    function send()
-    {
-     echo 'input masage';  // TODO: Implement send() method.
-    }
-}
-$look = new realise();
-$look->send();
-?>
 
 <?php
-interface area_calculation{
-    function ar_square();
-    function ar_rectangle();
-    function ar_circle();
-    function ar_trapezoid();
-    function ar_rhombus();
-    function ar_triangle();
+// число
+$number = "1547";
 
-}
-class areacalc implements area_calculation
-    public  $windth, $length, $radius, $height;
-    public function __constract($windth, $length, $radius, $height )
+// сюда будем записывать сумму
+$sum = 0;
+
+// получаем сумму цифр числа
+// доступ к каждой цифре числа можно получить через оператор доступа элементов массива
+for ($i = 0; $i < strlen($number); $i++)
 {
-        $this->windth = $windth;
-        $this->length = $length;
-        $this->radius = $radius;
-        $this->height = $height;
-    }
-    function ar_square()
-    {
-      echo 'square =';
-       echo $this->windth * $this->windth; // TODO: Implement ar_square() method.
-    }
-    function ar_rectangle(){
-        echo 'square rectangle =';
-        echo $this->windth * $this->length;
+    $sum += $number[$i];
 }
-    function ar_circle(){
-        echo 'square circle =';
-        echo 3.14 * ($this->radius * $this->radius);
-    }
-    function ar_trapezoid(){
-        echo 'square trapezoid =';
-        echo 0.5 * ($this->windth + $this->length) * $this->height;
-    }
+
+// выводим данные
+echo "Число: {$number}\n";
+echo "Сумма: {$sum}";
+?>
+<?php
+
+$n = "1425";
+$c = 0;
+
+for ($i = 0; $i < strlen($n); $i++);
+{
+    
+    $c += $n[$i];//return
+}
+echo $const;
 ?>
