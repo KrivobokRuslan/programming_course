a02cb9e306d891f87bc5f4660141108b83baebd2
krivobokruslan <krivobokruslan@gmail.com>
2023-03-17 20:06:57 +0200

    add class

--- oop/interfaces/1k.php
+++ oop/interfaces/1k.php
@@ -21,4 +21,49 @@ function enumeration($n, $x)
 //echo "Сумма: {$s}";
 enumeration ("14256585458354", 8);
 echo "<br>";
-?> //если блок вывода переставить после инициализации ф-ции, то дает ошибку
+
+class Enumaration {
+    public function __construct(private int $number, private int $numberForCount) {}
+
+    public function getSum(): int
+    {
+        $sum = 0;
+        $str = (string)$this->number;
+
+        for ($i = 0; $i < strlen($str); $i++) {
+            $sum += (int)$str[$i];
+        }
+
+        return $sum;
+    }
+
+    public function getCount(): int
+    {
+        $a = 0;
+        $str = (string)$this->number;
+
+        for ($i = 0; $i < strlen($str); $i++) {
+            if ( $str[$i]== $this->numberForCount) {
+                $a++;
+            }
+        }
+
+        return $a;
+    }
+}
+
+class Enumaration1 {
+    private int $number;
+    private int $numberForCount;
+    public function __construct(int $number, int $numberForCount) {
+        $this->number = $number;
+        $this->numberForCount = $numberForCount;
+    }
+}
+
+$enum = new Enumaration(23413454356234, 4);
+echo $enum->getSum();
+echo '<br>';
+echo $enum->getCount();
+
+?>
