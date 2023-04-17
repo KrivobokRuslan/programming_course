a02e4ff7ab7a5de9169e4b1e616e2786dcb78382
кот <ecopolimerdir@gmail.com>
2023-04-17 17:46:32 +0300

    commit message

---
+++ forms/PRIMER_K/performance/connect.php
@@ -0,0 +1,6 @@
+<?php
+//$connect = mysqli_connect('course-mysql', 'course_user','qwerty', 'course/primerusers');
+$pdo = new PDO('mysql:host=course-mysql;dbname=course/primerusers', 'course_user', 'qwerty');
+//if (!$connect){
+   // die('Error');
+//PRIMER_K/performance/connect.php
---
+++ forms/PRIMER_K/primer_authoriz.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link  rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <title> authoriz </title>
+</head>
+<body>
+<div class="col">
+    <h5 class="card-title">AUTHORIZATION  FORM</h5>
+</div>
+    <form  action ="" method = "post" width="400px">
+        <div class="mb-3">
+            <label for="login" class="form label">login</label>
+            <input type="text" class="form-control" id="login" placeholder="input login">
+        </div>
+        <div class="mb-3">
+            <label for="pass" class="form label">password</label>
+            <input type="password"class="form-control" id="pass" placeholder="input password" >
+        </div>
+        <button>Enter</button>
+        <p>
+            are you registered? <a href="/forms/PRIMER_K/primer_registration.php"> please register </a>
+        </p>
+    </form>
+</body>
+</html>
