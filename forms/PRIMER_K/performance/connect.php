1123af5dd59f21bd8763e18f6c7db2eba60fa7c7
кот <ecopolimerdir@gmail.com>
2023-04-19 18:57:03 +0300

    commit message

--- forms/PRIMER_K/performance/connect.php
+++ forms/PRIMER_K/performance/connect.php
@@ -1,7 +1,7 @@
 <?php
-$connect = mysqli_connect('course-mysql', 'course_user','qwerty', 'course/primerusers');
-//$pdo = new PDO('mysql:host=course-mysql;dbname=primerusers', 'course_user', 'qwerty');
-if (!$connect){
+//$connect = mysqli_connect('course-mysql', 'course_user','qwerty', 'course/primerusers');
+$pdo = new PDO('mysql:host=course-mysql;dbname=primerusers', 'course_user', 'qwerty');
+if (!$pdo){
     die('Error');
 }
 //PRIMER_K/performance/connect.php
