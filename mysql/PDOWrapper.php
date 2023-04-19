2d1e411d54a49a72ffe46e994e3b1db1d0bdcfc7
krivobokruslan <krivobokruslan@gmail.com>
2023-04-19 19:24:28 +0300

    add_wrapper

--- mysql/PDOWrapper.php
+++ mysql/PDOWrapper.php
@@ -2,5 +2,23 @@
 
 class PDOWrapper
 {
+    const MODE_FETCH = 1;
+    const MODE_FETCH_ALL = 2;
 
+    private PDO $pdo;
+    public function __construct() {
+        $this->pdo = new PDO('mysql:host=course-mysql;dbname=course', 'course_user', 'qwerty');
+    }
+    public function execute(string $sql, ?array $params = null, int $mode = self::MODE_FETCH) {
+        $result = $this->pdo->prepare($sql);
+        $result->execute($params);
+
+        if ($mode == self::MODE_FETCH) {
+            return $result->fetch(PDO::FETCH_ASSOC);
+        }
+
+        if ($mode == self::MODE_FETCH_ALL) {
+            return $result->fetchAll(PDO::FETCH_ASSOC);
+        }
+    }
 }
