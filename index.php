a57f89aafdfc6c69938ec58d1c98bcee3db30e97
krivobokruslan <krivobokruslan@gmail.com>
2023-11-24 19:21:37 +0200

    data types

--- index.php
+++ index.php
@@ -1,2 +1,21 @@
 <?php
-echo 'Hello Ruslan!';
+// string
+// integer
+// float
+// boolean (bool)
+// array
+// object
+// NULL
+// resource
+$a = 5;
+$b = 5;
+echo $a . $b; // + - * /
+$c = true;
+$d = false;
+$f = 'true';
+$g = 'true';
+$ar = [3, 4, 5, 67, 'asdgsdg', true];
+class A {
+}
+$v = new A();
+$i = null;
