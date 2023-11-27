ce52e14bfdb360318aefc6e6ac452b92df10f4db
krivobokruslan <krivobokruslan@gmail.com>
2023-11-27 19:31:05 +0200

    if tasks

--- forms/connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-
-$connect = mysqli_connect('course-mysql', 'course_user', 'qwerty', 'course');
-if (!$connect)
-    die('error!');
-//else
-    //echo 'Ok';
-?>
--- forms/employee_create.php
+++
@@ -1,7 +0,0 @@
-<?php
-require '../functions.php';
-require '../mysql/connect.php';
-$values = "'" . $_POST['full_name'] . "','". $_POST['date_of_birth'] . "','" . $_POST['position'] . "'," . $_POST['salary'];
-$insert = "INSERT INTO employee (`full_name`, `birth_date`, `position`, `salary`) VALUES ($values)";
-$pdo->query($insert);
-header('Location: ' . $_SERVER['HTTP_REFERER']);
